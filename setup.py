f970ff74cd668c15f028d31f0cff25869c96522f
Yana999 <abdramanova.yana@yandex.ru>
2022-06-20 15:41:23 +0300

    fix mistakes

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=("tests",)),
-    package_data={"book_recommender": ["VERSION"]},
+    package_data={"book_recommender": ["VERSION"], },
     install_requires=list_reqs(),
     extras_require={},
     include_package_data=True,
